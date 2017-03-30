8c9b9344260e60c0743f50c82070ea7153615c52
anotherrose98 <andrewmwilson98@gmail.com>
2017-03-30 14:47:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/wilsona/mastermind/EasyBoard.java
@@ -0,0 +1,13 @@
+package com.example.wilsona.mastermind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EasyBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_easy_board);
+    }
+}
