d55b7a84c19c745cca0a0e617de029be6b51e722
anotherrose98 <andrewmwilson98@gmail.com>
2017-04-11 14:23:17 -0400

    working on it

--- app/src/main/java/com/example/wilsona/mastermind/EasyBoard.java
+++ app/src/main/java/com/example/wilsona/mastermind/EasyBoard.java
@@ -3,44 +3,66 @@ package com.example.wilsona.mastermind;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class EasyBoard extends AppCompatActivity {
 
     private Spinner spinnerOne, spinnerTwo, spinnerThree, spinnerFour;
     private Button submit;
 
-    private int[] imgData = {R.drawable.greenpeg};
+    private int[] imgData = {R.drawable.greenpeg, R.drawable.redpeg};
 
+    private String[] imgNameData = { "green", "red"};
+
+    private HashMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_easy_board);
 
-        spinnerOne = (Spinner) findViewById(R.id.spnUserPegOne);
+        //spinnerOne = (Spinner) findViewById(R.id.spnUserPegOne);
         spinnerTwo = (Spinner) findViewById(R.id.spnUserPegTwo);
         spinnerThree = (Spinner) findViewById(R.id.spnUserPegThree);
         spinnerFour = (Spinner) findViewById(R.id.spnUserPegFour);
 
         submit = (Button) findViewById(R.id.btnSubmit);
 
-        CustomAdapter adapter = new CustomAdapter(getBaseContext(),R.layout.spinner_row,null,imgData);
+        CustomSpinnerAdapter adapter = new CustomSpinnerAdapter(getBaseContext(),R.layout.spinner_row,null,imgData);
         spinnerOne.setAdapter(adapter);
 
     }
 
 
     public void submit(View view) {}
+
+
+
+    private void initializeImageList() {
+        for (int i = 0; i < imgNameData.length; i++) {
+            map = new HashMap<String, Object>();
+
+            map.put("Name", imgNameData[i]);
+            map.put("Icon", imgData[i]);
+            spinnerOne.add(map);
+        }
+        ImageView imageView = new ImageView(this);
+        imageView.setBackgroundResource((spinnerOne.get(0).get("Icon")));
+        spinnerOne.get(0).get("Name");
+    }
+
+    public void createAddDialog() {
+        Spinner spin = (Spinner) findViewById(R.id.spnUserPegOne);
+        CustomSpinnerAdapter adapter = new CustomSpinnerAdapter(this,
+                spinnerData, R.layout.spinner_row, new String[] { "Name",
+                "Icon" }, new int[] { R.id.imageNameSpinner,
+                R.id.imageIconSpinner });
+        spin.setAdapter(adapter);
+    }
+
 
 }
 
